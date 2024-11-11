d63850765199723ebe1098a10fc301733bc0b8c7
MAHDYAR KARIMI <rhlkr7474@gmail.com>
2024-11-11 18:40:37 +0900

    p

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -22,7 +22,7 @@ const HeroContent = () => {
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <SparklesIcon className="text-[#b49bff] mr-[12px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
             Mahdyar Karimi`s Portfolio
           </h1>
