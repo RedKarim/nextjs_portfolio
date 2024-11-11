f880c8e666dfacb112b03874349d26e1a7e3853c
MAHDYAR KARIMI <rhlkr7474@gmail.com>
2024-11-11 18:35:02 +0900

    description

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -23,9 +23,7 @@ const HeroContent = () => {
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
-          </h1>
+          <h1 className="Welcome-text text-[13px]">My Development Portfolio</h1>
         </motion.div>
 
         <motion.div
@@ -33,12 +31,12 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+            Always
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              trying to LEARN{" "}
             </span>
-            project exprience
+            new things
           </span>
         </motion.div>
 
@@ -46,8 +44,9 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          I&apos;m AWS Certified front-end developer skilled in web development.
+          Passionate about blockchain, machine learning, and creating impactful
+          digital solutions.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
