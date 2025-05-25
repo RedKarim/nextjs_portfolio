bec2e53141092ca99cadaf0eee11c2a087eaae69
RedKarim <rhlkr7474@gmail.com>
2025-05-25 14:41:30 +0900

    refactor: improving the layout of Cert

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -17,6 +17,7 @@ const Hero = () => {
           autoPlay
           muted
           loop
+          playsInline
           className="rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover"
         >
           <source src="/blackhole.webm" type="video/webm" />
